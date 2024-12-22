d423b7513bb3ea1bd7f2d93c1ebdf994c60a3604
Oleksii <copalex131@gmail.com>
2024-12-22 09:06:52 +0200

    Calculation of the sum of numbers and output of the result have been added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5+10={}",x + y);
+            Console.ReadKey();
+        }
+    }
+}
